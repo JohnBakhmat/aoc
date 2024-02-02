c225ef7b0754f548b5e5c3132da08c76f02d2e7f
John Bakhmat <johnbakhmat@gmail.com>
2024-02-02 14:21:14 +0300

    inlining in day 02 2002

--- 2022/day02/ocaml/bin/main.ml
+++ 2022/day02/ocaml/bin/main.ml
@@ -10,11 +10,7 @@ let round_resolution a b =
 
 let move_to_score m = match m with "X" -> 1 | "Y" -> 2 | "Z" -> 3 | _ -> 0
 let result_to_score r = match r with Win -> 6 | Tie -> 3 | Loose -> 0
-
-let rec sum l =
-  match l with
-  | [] -> 0
-  | x :: tail -> ( match x with Some n -> n + sum tail | None -> 0)
+let rec sum l = match l with [] -> 0 | x :: tail -> x + sum tail
 
 let () =
   let results =
@@ -22,11 +18,10 @@ let () =
     |> String.split_on_char '\n'
     |> List.filter (fun s -> s <> "")
     |> List.map (fun s ->
-           let moves = String.split_on_char ' ' s in
-           match moves with
+           match String.split_on_char ' ' s with
            | a :: b :: _ ->
-               Some (result_to_score (round_resolution a b) + move_to_score b)
-           | _ -> None)
+               result_to_score (round_resolution a b) + move_to_score b
+           | _ -> 0)
   in
 
   print_int (sum results)
